196d4dec08a5fa486edf0e8ed3e278b694860864
kdxiaoyi [MiPad7] <kdxiaoyi@outlook.com>
2026-02-28 22:10:52 +0800

    添加前端按钮到命令的自动映射

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,14 @@
 import titles from "./ui/titles.js";
+import commands from './backend/commands.js';
 import i18n from './i18n.js';
 
 // 加载UI和i18n
 i18n.refresh();
 titles.setTitle();
+
+// 注册前端按钮到命令的映射
+document.querySelectorAll("*[data-click]").forEach((e) => {
+  e.addEventListener("click", (event) => {
+    commands.executeCommand(e.dataset.click, event);
+  });
+})
