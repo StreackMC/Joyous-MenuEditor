fcd5bb90a58af8894f7ba555c8fd79e4202391eb
kdxiaoyi [MiPad7] <kdxiaoyi@outlook.com>
2026-02-28 20:39:39 +0800

    添加加载错误提示

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,7 @@
 import titles from "./ui/titles.js";
 import i18n from './i18n.js';
-import { currentTranslations } from './i18n.js';
 
 // 加载UI和i18n
-await i18n.load("zh_cn");
 i18n.refresh();
-if (currentTranslations.product) { };
 titles.setTitle();
 
-// 加载完毕后移除遮罩
-document.getElementById("loading").remove();
