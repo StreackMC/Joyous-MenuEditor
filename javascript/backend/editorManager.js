c695daeceff08b99684825dbc2ee2cd46afceb18
kdxiaoyi [MiPad7] <kdxiaoyi@outlook.com>
2026-03-14 18:05:46 +0800

    优化指定编辑器的打开流程

--- javascript/backend/editorManager.js
+++ javascript/backend/editorManager.js
@@ -39,16 +39,31 @@ export function regisiterEditor(id, varify, clazz) {
  * 打开一个编辑器，智能识别打开方式
  * @param {*} data 数据
  * @param {string} editorId 打开方式
- * @param {string} fname 编辑器标题，建议传入文件名，最终由实际的编辑器决定
- * @throws 找不到目标编辑器
+ * @param {string} fname 编辑器标题，建议传入文件名，最终由实际的编辑器依据此参数决定；指定了打开方式时且目标编辑器没有返回有效标题时则改为使用此参数。
+ * @throws 找不到目标编辑器，若没有指定编辑器抛出此错误时表示默认的 ACE 编辑器都不可用
  */
 export function openEditor(data = "", editorId = undefined, fname = null) {
   fname = (fname) ? fname : `Untitled-${getUntitledId()}`;
-  if (editorId) {
-    // 如果指定了打开方式则直接打开
-    const clazz = regEditorsClazz.get(editorId);
-    tabs.openTab(new clazz(data, fname), fname);//不catch潜在的null，由调用者自行处理
-  };
+
+  // 如果指定了打开方式则直接打开
+  try {
+    if (editorId) {
+      const clazz = regEditorsClazz.get(editorId);
+      const func = regEditorsVarify.get(editorId);
+      let title;
+      try {
+        title = func(this, fname);
+        if (!title) { throw new Error("目标编辑器不支持打开此类文件"); };
+      } catch (error) {
+        console.warn(`强制使用编辑器 ${editorId} 时，无法获取编辑器标题：`, error, "\n 目标数据：", data);
+        title = fname;
+      }
+      tabs.openTab(new clazz(data, fname), title);//不catch潜在的null，由调用者自行处理
+    };
+  } catch (error) {
+    throw new Error(i18n.parseSafe("msg.unknownEditor", { editor: editorId }));
+  }
+
   // 判断打开方式
   try {
     regEditorsVarify.forEach((value, key) => {
